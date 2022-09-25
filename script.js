ba11386a5e3c75b7a973fc3d7b05870113bd4ed9
Dinesh <dinesh.skini@gmail.com>
2022-09-24 20:30:01 -0400

    feat(shop) CSS and corosal changes

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ $('.navTrigger').click(function () {
   $('#mainListDiv').toggleClass('show_list');
   $('#mainListDiv').fadeIn();
 });
+
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses();
+    panel.classList.add('active');
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
+}
